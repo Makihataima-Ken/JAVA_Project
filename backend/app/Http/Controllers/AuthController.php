696e79d226e30956f8c5c258c6d8ac9e3d9ebf55
Ken <ahmadsoud63@gmail.com>
2024-10-02 10:56:53 +0300

    login function made

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -19,12 +20,40 @@ class AuthController extends Controller
         ]);
         //wrong input
         if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 422);
+            return response()->json($validator->errors()->toJson(), 400);
         }
         //register user
         $user=User::create(array_merge([$validator->validated(),'password'=>bcrypt($request->password)]));
 
         return response()->json(['message'=>'registered successfully','user'=>$user],201);
 
+    }
+
+    public function login(Request $request)
+    {
+        //validation
+        $validator = Validator::make($request->all(), [
+            'phone'=>'required|string|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        //wrong input
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        if(!$token=Auth::attempt($validator->validated()))
+        {
+        return response()->json(['error'=>'unauthorized'],401);
+        }
+        return $this->createNewToken($token);
+    }
+
+    public function createNewToken($token)
+    {
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>Auth::factory()->getTTl()*60,
+            'user'=>Auth::user(),
+        ]);
     }
 }
