ec2fafdcd4dff489741cc53c23c8940402a511a5
Ken <ahmadsoud63@gmail.com>
2024-10-09 10:58:42 +0300

    made another test to figuer where the problem is

---
+++ backend/tests/Feature/OrderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\JsonResponse;
+
+uses(RefreshDatabase::class);
+
+test('ordering', function () {
+    $response = $this->postJson('/add_order', [
+        'university' => 'Damas',
+        'major' => 'med',
+        'type' => 'grad pro',
+        'description'=>'smth smth',
+        'deadline'=>'1/8/2024',
+    ]);
+
+    $response->assertStatus(JsonResponse::HTTP_CREATED)
+    ->assertJson([
+        'message' => 'added an order',
+        'order' => [
+            'university' => 'Damas',
+            'major' => 'med',
+            'type' => 'grad pro',
+            'description'=>'smth smth',
+            'deadline'=>'1/8/2024',
+            'status'=>'pending',
+        ],
+    ]);
+    
+    $this->assertDatabaseHas('orders');
+});
