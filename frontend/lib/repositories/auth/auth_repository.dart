3f35a0c75bd0329aa6c3dbe621359ae0f19e6083
Jaafer Mahfoud <jaafer.mafhoud97@gmail.com>
2024-10-22 08:29:53 +0300

    cleaned the cod ea little bit

--- frontend/lib/repositories/auth/auth_repository.dart
+++ frontend/lib/repositories/auth/auth_repository.dart
@@ -3,7 +3,7 @@ import "package:frontend/models/models.dart";
 import "package:frontend/models/requests/requests.dart";
 import 'package:frontend/repositories/auth/base_auth_repository.dart';
 import "package:frontend/repositories/core/endpoints.dart";
-import "package:frontend/utils/dio_client/dio_client.dart";
+import "package:frontend/utils/utils.dart";
 
 class AuthRepository extends BaseAuthRepository {
   AuthRepository({
@@ -19,24 +19,12 @@ class AuthRepository extends BaseAuthRepository {
       data: request.toJson(),
     );
 
-
     return AppResponse<AuthUser?>.fromJson(
       response.data,
       (dynamic json) {
         if (json != null) {
-          Map userjs = json['user'];
-          UserEntity user = UserEntity(
-              name: userjs['name'],
-              lastname: userjs['lastname'],
-              phone: userjs['phone'],
-              usertype: userjs['usertype'],
-              createdAt:
-                  DateTime.tryParse(userjs['created_at']) ?? DateTime.now(),
-              updatedAt:
-                  DateTime.tryParse(userjs['updated_at']) ?? DateTime.now(),
-              id: userjs['id']);
           return AuthUser(
-            user: user,
+            user: extractUserFromJson(json['user']),
             token: json['access_token'],
             tokenType: json['token_type'],
             expiresIn: json['expires_in'],
@@ -61,24 +49,12 @@ class AuthRepository extends BaseAuthRepository {
       data: request.toJson(),
     );
 
-
     return AppResponse<AuthUser?>.fromJson(
       response.data,
       (dynamic json) {
         if (json != null && json is Map<String, dynamic>) {
-          Map userjs = json['user'];
-          UserEntity user = UserEntity(
-              name: userjs['name'],
-              lastname: userjs['lastname'],
-              phone: userjs['phone'],
-              usertype: userjs['usertype'],
-              createdAt:
-                  DateTime.tryParse(userjs['created_at']) ?? DateTime.now(),
-              updatedAt:
-                  DateTime.tryParse(userjs['updated_at']) ?? DateTime.now(),
-              id: userjs['id']);
           return AuthUser(
-            user: user,
+            user: extractUserFromJson(json['user']),
             token: response.data['access_token'] ?? '',
             tokenType: response.data['token_type'] ?? '',
             expiresIn: response.data['expires_in'] ?? 0,
--- frontend/lib/utils/utils.dart
+++ frontend/lib/utils/utils.dart
@@ -1,3 +1,4 @@
 export "dio_client/app_interceptors.dart";
 export "dio_client/dio_client.dart";
 export "token_storage/token_storage.dart";
+export "custom_from_json/custom_from_json.dart";
