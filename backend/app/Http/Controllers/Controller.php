692292e23138c69f3e3d7ab1653fdfa734354aea
Ken <ahmadsoud63@gmail.com>
2024-10-23 17:52:45 +0300

    made a prompt function to send responses

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -1,9 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\JsonResponse;
 
 
 abstract class Controller 
 {
-    //
+    public function pass($success,$message,$data=null,$statuscode):JsonResponse
+    {
+        return response()->json([
+            'success'=>$success,
+            'message'=>$message,
+            'data'=>$data,
+        ],$statuscode);
+    }
+
+    public function error($success,$message,$errors=null,$statuscode):JsonResponse
+    {
+        return response()->json([
+            'success'=>$success,
+            'message'=>$message,
+            'errors'=>$errors,
+        ],$statuscode);
+    }
 }
