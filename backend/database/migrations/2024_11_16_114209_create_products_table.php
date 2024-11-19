c822f8bec06be86e97a13cf56ed2c539366188fd
Ken <ahmadsoud63@gmail.com>
2024-11-19 16:01:42 +0300

    edited product model

--- backend/database/migrations/2024_11_16_114209_create_products_table.php
+++ backend/database/migrations/2024_11_16_114209_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('file_path');
             $table->timestamps();
